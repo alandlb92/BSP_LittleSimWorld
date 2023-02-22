ca4610b73d054230845630c0649699fdc343ce8c
Alan Bittencourt <alan.dlucas.bittencourt@gmail.com>
2023-02-21 23:01:58 -0300

    inventory

--- Assets/Scripts/Character/Controllers/CharacterInventoryController.cs
+++ Assets/Scripts/Character/Controllers/CharacterInventoryController.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CharacterInventoryController
+public class CharacterInventoryController : ICharacterInventory
 {
     private CharacterInventoryData _data;
     private IPlayerHUD _HUD;
